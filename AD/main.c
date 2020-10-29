eb5dbfa23d9fe1f4cb1dcc873498754f0e406023
Aria Delmar <akd17@my.fsu.edu>
2020-10-29 14:24:17 -0400

    Create main.c

---
+++ AD/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    while(1){
+    char* input = get_input();
+    if(strcmp(input, "exit") == 0)
+        break; 
+    }
+
+    return 0; 
+}
