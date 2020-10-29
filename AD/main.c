be4d24a762f3de271268b3e078e115f3854f4dbf
Aria Delmar <akd17@my.fsu.edu>
2020-10-29 14:43:56 -0400

    starting point: exit

--- AD/main.c
+++ AD/main.c
@@ -1,5 +1,12 @@
+#define _POSIX_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+char *get_input(void); 
 
 int main(){
 
@@ -10,4 +17,32 @@ int main(){
     }
 
     return 0; 
+}
+
+char *get_input(void)
+{
+	char *buffer = NULL;
+	int bufsize = 0;
+
+	char line[5];
+	while (fgets(line, 5, stdin) != NULL) {
+		int addby = 0;
+		char *newln = strchr(line, '\n');
+		if (newln != NULL)
+			addby = newln - line;
+		else
+			addby = 5 - 1;
+
+		buffer = (char *) realloc(buffer, bufsize + addby);
+		memcpy(&buffer[bufsize], line, addby);
+		bufsize += addby;
+
+		if (newln != NULL)
+			break;
+	}
+
+	buffer = (char *) realloc(buffer, bufsize + 1);
+	buffer[bufsize] = 0;
+
+	return buffer;
 }
