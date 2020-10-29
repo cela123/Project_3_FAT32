51c8fd27961aff69c1179a2fa28560fe553702c2
Aria Delmar <akd17@my.fsu.edu>
2020-10-29 15:34:34 -0400

    info function

--- AD/main.c
+++ AD/main.c
@@ -6,19 +6,33 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+void info(int, int, int, int, int, int, int);
 char *get_input(void); 
+
 
 int main(){
 
     while(1){
         char* input = get_input();
+
         if(strcmp(input, "exit") == 0){
             free(input);
             break; 
         }
+        if(strcmp(input, "info") == 0){
+
+            info(1,2,3,4,5,6,7); 
+        }
     }
     
     return 0; 
+}
+
+void info(int bps, int spc, int rsc, int noF, int totS, int szF, int rc){
+  //  char* test = "bytes per sector: %d\nsectors per cluster: %d\nreseverd sector count: %d\nnumber of FATs: %d\ntotal sectors: %d\nFATsize: %d\nroot cluster: %d\n"; 
+    printf("bytes per sector: %d\nsectors per cluster: %d\nreseverd sector count: %d\nnumber of FATs: %d\ntotal sectors: %d\nFATsize: %d\nroot cluster: %d\n", bps, spc, rsc, noF, totS, szF, rc);
+   // char* infoStr = sprintf( "bytes per sector: %d\nsectors per cluster: %d\nreseverd sector count: %d\nnumber of FATs: %d\ntotal sectors: %d\nFATsize: %d\nroot cluster: %d\n", bps, spc, rsc, noF, totS, szF, rc); 
+   // return infoStr
 }
 
 char *get_input(void)
