350b7337a4bba95a8c8b2d47efb9ec04718008e3
Aria Delmar <akd17@my.fsu.edu>
2020-11-01 14:32:03 -0500

    parser changes

--- AD/main.c
+++ AD/main.c
@@ -5,9 +5,9 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include "parser.h"
 
 void info(int, int, int, int, int, int, int);
-char *get_input(void); 
 
 
 int main(){
@@ -15,12 +15,13 @@ int main(){
     while(1){
         printf("$ ");
         char* input = get_input();
+		tokenlist *inputTokens = get_tokens(input);
 
-        if(strcmp(input, "exit") == 0){
+        if(strcmp(inputTokens->items[0], "exit") == 0){
             free(input);
             break; 
         }
-        if(strcmp(input, "info") == 0){
+        if(strcmp(inputTokens->items[0], "info") == 0){
 
             info(1,2,3,4,5,6,7); 
         }
@@ -35,30 +36,30 @@ void info(int bps, int spc, int rsc, int noF, int totS, int szF, int rc){
 
 }
 
-char *get_input(void)
-{
-	char *buffer = NULL;
-	int bufsize = 0;
+// char *get_input(void)
+// {
+// 	char *buffer = NULL;
+// 	int bufsize = 0;
 
-	char line[5];
-	while (fgets(line, 5, stdin) != NULL) {
-		int addby = 0;
-		char *newln = strchr(line, '\n');
-		if (newln != NULL)
-			addby = newln - line;
-		else
-			addby = 5 - 1;
+// 	char line[5];
+// 	while (fgets(line, 5, stdin) != NULL) {
+// 		int addby = 0;
+// 		char *newln = strchr(line, '\n');
+// 		if (newln != NULL)
+// 			addby = newln - line;
+// 		else
+// 			addby = 5 - 1;
 
-		buffer = (char *) realloc(buffer, bufsize + addby);
-		memcpy(&buffer[bufsize], line, addby);
-		bufsize += addby;
+// 		buffer = (char *) realloc(buffer, bufsize + addby);
+// 		memcpy(&buffer[bufsize], line, addby);
+// 		bufsize += addby;
 
-		if (newln != NULL)
-			break;
-	}
+// 		if (newln != NULL)
+// 			break;
+// 	}
 
-	buffer = (char *) realloc(buffer, bufsize + 1);
-	buffer[bufsize] = 0;
+// 	buffer = (char *) realloc(buffer, bufsize + 1);
+// 	buffer[bufsize] = 0;
 
-	return buffer;
-}
+// 	return buffer;
+// }
