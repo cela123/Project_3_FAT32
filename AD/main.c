e57fd86d1602ba1b35d232c25652c523f2cbbdae
Aria Delmar <akd17@my.fsu.edu>
2020-11-05 17:54:21 -0500

    Update main.c

--- AD/main.c
+++ AD/main.c
@@ -67,31 +67,3 @@ void print_info(int bps, int spc, int rsc, int noF, int totS, int szF, int rc){
     printf("bytes per sector: %d\nsectors per cluster: %d\nreserved sector count: %d\nnumber of FATs: %d\ntotal sectors: %d\nFATsize: %d\nroot cluster: %d\n", bps, spc, rsc, noF, totS, szF, rc);
 
 }
-
-// char *get_input(void)
-// {
-// 	char *buffer = NULL;
-// 	int bufsize = 0;
-
-// 	char line[5];
-// 	while (fgets(line, 5, stdin) != NULL) {
-// 		int addby = 0;
-// 		char *newln = strchr(line, '\n');
-// 		if (newln != NULL)
-// 			addby = newln - line;
-// 		else
-// 			addby = 5 - 1;
-
-// 		buffer = (char *) realloc(buffer, bufsize + addby);
-// 		memcpy(&buffer[bufsize], line, addby);
-// 		bufsize += addby;
-
-// 		if (newln != NULL)
-// 			break;
-// 	}
-
-// 	buffer = (char *) realloc(buffer, bufsize + 1);
-// 	buffer[bufsize] = 0;
-
-// 	return buffer;
-// }
