b8045486b8c31c3874746e37a66ce5795fb80727
amico <alecamico123@yahoo.com>
2020-11-25 10:47:15 -0500

    directoryentry copy function

--- AA/tmain.c
+++ AA/tmain.c
@@ -9,11 +9,12 @@
 #include <unistd.h>
 #include "parser.h"
 
+//Structs
 typedef struct bpb_info_struct
 {
-    unsigned short bpb_bytspersec, bpb_secperclus,
-                  bpb_rsvdseccnt, bpb_numfats;
-    unsigned int bpb_fatsz32, bpb_rootclus, bpb_totsec32;
+  unsigned short bpb_bytspersec, bpb_secperclus,
+                bpb_rsvdseccnt, bpb_numfats;
+  unsigned int bpb_fatsz32, bpb_rootclus, bpb_totsec32;
 }Bpb_info_struct;
 
 typedef struct { // DIRECTORY ENTRY
@@ -29,22 +30,22 @@ typedef struct { // DIRECTORY ENTRY
     unsigned short DIR_WrtTime;         //offset byte 22, do not care
     unsigned short DIR_WrtDate;         //offset byte 24, do not care
     unsigned short DIR_FstClusLo;       //offset byte 26
-    unsigned int DIR_FileSize;        //offset byte 28
+    unsigned short DIR_FileSize;        //offset byte 28
 } __attribute__((packed)) DIR_ENTRY;
 
 Bpb_info_struct bpb_information;
+
+//Function Declarations
 void gather_info(int fd);
 void print_info();
 int isCommand(char *);
-int create(char* name, int dirCheck, int fd);
-void fill_dir_entry(DIR_ENTRY* entry, int dir, int fd);
 
 int dir_cluster_num(int fd, char dirName[11], int curDir, int firstDataLoc, int curCluster);
 int next_cluster_num(int fd, unsigned int currentClusterNum);
 unsigned int find_empty_cluster(int fd);
 
 
-
+//MAIN STARTS
 int main(){
     off_t temp;
     ssize_t temp2;
@@ -65,8 +66,6 @@ int main(){
         printf("Error opening file");
 
     gather_info(fd);
-    DIR_ENTRY tempdir[65534];
-
 
     dataRegStart = bpb_information.bpb_bytspersec*(bpb_information.bpb_rsvdseccnt + (bpb_information.bpb_numfats*bpb_information.bpb_fatsz32));
     //set current directory to the beginning of data region intially (ie start in root)
@@ -74,8 +73,7 @@ int main(){
     currDirectoryCluster = bpb_information.bpb_rootclus;
     printf("Data Region Start = %d\n", dataRegStart);
     printf("Root Cluster = %d\n", currDirectoryCluster);
-    dir_cluster_num(fd, "BLUE", currDirectory, dataRegStart, currDirectoryCluster);
-    fill_dir_entry(tempdir, 0x100460, fd);
+
     while(1){
 
         printf("$ ");
@@ -287,11 +285,15 @@ void gather_info(int fd)
     temp_ssize_t = read(fd, &bpb_information.bpb_rootclus, 4);
 }
 
+
+
 void print_info(){
+
     printf("bpb_bytspersec: %d\nbpb_secperclus: %d\nbpb_rsvdseccnt: %d\nbpb_numfats: %d\nbpb_totsec32: %d\nbpb_fatsz32: %d\nbpb_rootclus: %d\n",
     bpb_information.bpb_bytspersec, bpb_information.bpb_secperclus, bpb_information.bpb_rsvdseccnt,
     bpb_information.bpb_numfats, bpb_information.bpb_totsec32, bpb_information.bpb_fatsz32,
     bpb_information.bpb_rootclus);
+
 }
 
 int isCommand(char * userCmd){
@@ -406,7 +408,7 @@ int dir_cluster_num(int fd, char dirName[11], int curDir, int firstDataLoc, int 
 
 /*
     Function: next_cluster_num
-    Navigates the FAT region to determine the cluster that comes after the current cluster
+    Navigates the FAT region to determine the cluster that comes after the current ccluster
     Returns the next cluster number
     Will return -1 if current cluster is the last cluster
 */
@@ -428,29 +430,6 @@ int next_cluster_num(int fd, unsigned int currentClusterNum){
     }
 }
 
-void fill_dir_entry(DIR_ENTRY* entry, int dir, int fd){
-    off_t temp;
-    ssize_t temp2;
-    temp = lseek(fd, dir, SEEK_SET);
-
-    temp2 = read(fd, &entry->DIR_Name, 11);
-    entry->DIR_Name[10] = '\0';
-    temp2 = read(fd, &entry->DIR_Attributes, 1);
-    temp = lseek(fd, 8, SEEK_CUR);
-    temp2 = read(fd, &entry->DIR_FstClusHI, 2);
-    temp = lseek(fd, 4, SEEK_CUR);
-    temp2 = read(fd, &entry->DIR_FstClusLo, 2);
-    temp2 = read(fd, &entry->DIR_FileSize, 4);
-
-    printf("Inside fill_dir_entry\nDIR_Name: %s\nDIR_Attributes: %d\nDIR_FstClusHI: %d\nDIR_FstClusLo: %d\nDIR_FileSize: %d\n",
-    entry->DIR_Name, entry->DIR_Attributes, entry->DIR_FstClusHI, entry->DIR_FstClusLo, entry->DIR_FileSize);
-
-}
-
-int create(char* name, int dirCheck, int fd){
-    DIR_ENTRY * temp;
-}
-
 unsigned int find_empty_cluster(int fd){
     int emptyCluster = 0;
     off_t temp;
@@ -465,5 +444,6 @@ unsigned int find_empty_cluster(int fd){
     }
 
     return emptyCluster;
+
 
 }
